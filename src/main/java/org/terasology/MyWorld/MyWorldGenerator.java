ccd4687fed8e2c7181aab660aa7932f06cd9bbb4
Sohil <sohil.khan19may@gmail.com>
2018-01-10 18:08:24 +0530

    myWorldGenerator

---
+++ src/main/java/org/terasology/MyWorld/MyWorldGenerator.java
@@ -0,0 +1,29 @@
+package org.terasology.MyWorld;
+
+import org.terasology.core.world.generator.facetProviders.SeaLevelProvider;
+import org.terasology.engine.SimpleUri;
+import org.terasology.registry.In;
+import org.terasology.world.generation.BaseFacetedWorldGenerator;
+import org.terasology.world.generation.WorldBuilder;
+import org.terasology.world.generator.RegisterWorldGenerator;
+import org.terasology.world.generator.plugin.WorldGeneratorPluginLibrary;
+
+@RegisterWorldGenerator(id = "myWorld", displayName = "My World")
+public class MyWorldGenerator extends BaseFacetedWorldGenerator {
+
+    @In
+    private WorldGeneratorPluginLibrary worldGeneratorPluginLibrary;
+
+    public MyWorldGenerator(SimpleUri uri) {
+        super(uri);
+    }
+
+    @Override
+    protected WorldBuilder createWorld() {
+        return new WorldBuilder(worldGeneratorPluginLibrary)
+                .addProvider(new SurfaceProvider())
+                .addProvider(new SeaLevelProvider(0))
+                .addRasterizer(new MyWorldRasterizer());
+
+    }
+}
